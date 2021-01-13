9a7151b36060b91efdf657c4c43ba6b5950d3fe4
martycabj1999 <martingira1999@gmail.com>
2021-01-13 19:28:47 -0300

    init

---
+++ database/migrations/2014_10_11_000000_create_purchases_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->float('mount');
+            $table->string('code');
+            $table->boolean('verified');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
